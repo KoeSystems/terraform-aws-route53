c63be1351d4b5c6bbedbe922991797dc6825dac3
Koe Pena de San Victor <koe@YELP-KOE.local>
2015-09-16 23:13:24 +0100

    Add domain output to public

--- public/outputs.tf
+++ public/outputs.tf
@@ -1,3 +1,4 @@
+output "domain" { value = "${var.domain}" }
 output "zone_id" { value = "${aws_route53_zone.domain.zone_id}" }
 output "subdomain_zone_ids" { value = "${join(",",aws_route53_zone.subdomain.*.zone_id)}" }
 output "subdomain_names"    { value = "${var.subdomains}" }
