0070ca3815a1001df0123f38ee3605c2951eb79a
Koe <koe.systems@gmail.com>
2017-10-13 15:09:59 +0100

    Add descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,2 @@
-output "domain_name"            { value = "${var.domain_name}" }
-output "primary_public_zone_id" { value = "${aws_route53_zone.primary_public.zone_id}" }
+output "domain_name"            { value = "${var.domain_name}"                         description = "Domain name for the hosted zoned in R53." }
+output "primary_public_zone_id" { value = "${aws_route53_zone.primary_public.zone_id}" description = "R53 hosted zone ID." }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,1 @@
-variable "domain_name" { }
+variable "domain_name" { description = "Route53 Domain Name" }
